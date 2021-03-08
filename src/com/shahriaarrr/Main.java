23f38ad21fa670e03f9cfce970e7204e988fa5b5
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-03-08 15:19:41 +0330

    get number from user

--- src/com/shahriaarrr/Main.java
+++ src/com/shahriaarrr/Main.java
@@ -7,5 +7,11 @@ public class Main {
     public static void main(String[] args) {
 	    int a, b;
 	    Scanner in = new Scanner(System.in);
+
+        System.out.print("give me your first number: ");
+        a = in.nextInt();
+
+        System.out.print("give me your second number: ");
+        b = in.nextInt();
     }
 }
